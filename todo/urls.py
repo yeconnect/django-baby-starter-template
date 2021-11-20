46e97972a15312e511c949274bf5f6c49818a97d
稗田直也 <naoyahieda@gmail.com>
2021-11-20 21:08:42 +0900

    .queryを付ける方法を試した

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.urls import path
-from .views import todo_all
+from .views import todo_all, todo_all_with_query
 
 urlpatterns = [
     path('',todo_all,name="todo_all"),
+    path('query',todo_all_with_query,name="query"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -4,4 +4,8 @@ from .models import Todo
 
 def todo_all(request):
   all_todos = Todo.objects.all()
-  return HttpResponse(f'<h1>{len(all_todos)}件ありました。</h1>')
+  return HttpResponse(f'<h1>{len(all_todos)}件ありました。</h1><h2>ターミナルにSQLが表示されています。</h2>')
+
+def todo_all_with_query(request):
+  print("発行されたSQL",Todo.objects.all().query)
+  return HttpResponse(f'.queryを使う方法です。ターミナルにSQLが表示されています。</h2>')
