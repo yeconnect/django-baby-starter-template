8caaf00f4e91a957d7ab47b724bc0847d6a52107
稗田直也 <naoyahieda@gmail.com>
2021-11-29 21:14:03 +0900

    transaction added

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.urls import path
-from .views import todo_all, todo_all_with_query
+from .views import todo_all, todo_all_with_query, transaction_view, without_transaction
 
 urlpatterns = [
     path('',todo_all,name="todo_all"),
     path('query',todo_all_with_query,name="query"),
+    path('without_transaction',without_transaction,name="withou-transaction"),
+    path('with_transaction',transaction_view,name="transaction"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.http.response import Http404, HttpResponse
 from django.shortcuts import render
 from .models import Todo
+from django.db import transaction
 
 def todo_all(request):
   all_todos = Todo.objects.all()
@@ -9,3 +10,16 @@ def todo_all(request):
 def todo_all_with_query(request):
   print("発行されたSQL",Todo.objects.all().query)
   return HttpResponse(f'.queryを使う方法です。ターミナルにSQLが表示されています。</h2>')
+
+def without_transaction(request):
+  print('トランザクションなし')
+  Todo.objects.create(title="トランザクションなしの場合",content="これは残る")
+  raise Exception('わざとエラーを生じさせる')
+  return HttpResponse('Good')
+
+def transaction_view(request):
+  print("トランザクションあり")
+  with transaction.atomic():
+    Todo.objects.create(title="トランザクションありの場合",content="これは残らない")
+    raise Exception('わざとエラーを生じさせる')
+  return HttpResponse('Good')
