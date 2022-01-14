5fd66faf8d2221d42418669aecd6d06b0a128ad8
稗田直也 <naoyahieda@gmail.com>
2022-01-14 20:41:43 +0900

    生コマンド

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+SEX = (
+		('male', '男性'),
+		('female', '女性'),
+		('other', 'その他'),
+		
+	)
 class BlogUser(models.Model):
     """ ユーザ(適当、認証など考えるとDjangoデフォルトのモデルが良さそう) """
     name = models.CharField(max_length=30)
     email = models.EmailField()
     following = models.ManyToManyField("self",related_name="followed_by",symmetrical=False,blank=True)
+    sex = models.CharField(max_length=10, choices=SEX, unique=True, default='')
 
     def __str__(self):
         return self.name
