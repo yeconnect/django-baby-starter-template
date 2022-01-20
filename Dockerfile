544ef144ab0ee65cca38c0b2d534059aedf689d6
稗田直也 <naoyahieda@gmail.com>
2022-01-20 17:08:51 +0900

    production分岐

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# 本番用
 FROM python:3.9
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
@@ -5,3 +6,7 @@ WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
 ADD . /code/
+
+EXPOSE 8002
+
+CMD ["gunicorn", "--reload", "-b", ":8002", "config.wsgi:application","-w","2","-k","gevent"]
---
+++ config/settings/local.py
@@ -0,0 +1,19 @@
+from .common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0=limiw24v7#rnr(cf!3p0akvgz0)lgw0$_ri5%))rim=+!g1q'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+ALLOWED_HOSTS = ["*"]
