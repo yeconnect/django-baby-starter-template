5150751b08e9a284cabcef1c60ee5bd160fd6cae
稗田直也 <naoyahieda@gmail.com>
2022-02-10 15:59:55 +0900

    manage.py changed for migration

--- manage.py
+++ manage.py
@@ -6,7 +6,13 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    if os.environ.get('ENV') == "PRODUCTION":
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              'config.settings.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              'config.settings.local')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
