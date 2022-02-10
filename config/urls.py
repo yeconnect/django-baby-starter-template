bc2bed6c8c1bc923d2c9c0b550743e1395c82e09
稗田直也 <naoyahieda@gmail.com>
2022-02-10 16:33:00 +0900

    migrate

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.core import management
 from django.http import HttpResponse
 from django.urls import path
+
+
+def migrate_func(request):
+    management.call_command('migrate')
+    return HttpResponse('Migrated')
 
 
 def health_func(request):
@@ -25,5 +31,6 @@ def health_func(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('healthz', health_func)
+    path('healthz', health_func),
+    path('migrate', migrate_func)
 ]
