ce8e394b7277848682f423b712280865da231d0a
檜原陽一郎 <hibarayoichiro871@gmail.com>
2022-02-19 14:21:21 +0900

    決済画面移動時のDB書き込み

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# 決済履歴データベース: 親のユーザID, プランの種類, 決済IDを紐付け
+# プランは後から追加可能に
+
+class Plan(models.Model):
+    plan_name = models.CharField(max_length=32, verbose_name="プランの種類")
+    
+    def __str__(self):
+        return self.plan_name
+         
+    class Meta:
+        verbose_name = 'プランの種類'
+        verbose_name_plural = 'プランの種類'
+        
+class PaymentHistory(models.Model):
+    uid = models.CharField(max_length=32, verbose_name="ユーザID")
+    plan = models.OneToOneField(Plan, on_delete=models.PROTECT, verbose_name="プランの種類")
+    merchantPaymentId = models.CharField(max_length=32, primary_key=True, verbose_name="決済ID")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="決済画面移動時刻")
+    payed_at = models.DateTimeField(null=True, blank=True, verbose_name="決済完了時刻")
+    
+    def __str__(self):
+        return self.merchantPaymentId
+    
+    class Meta:
+        verbose_name = '決済履歴'
+        verbose_name_plural = '決済履歴'
--- bookstore/views.py
+++ bookstore/views.py
@@ -6,25 +6,36 @@ from PIL import Image
 import qrcode
 import base64
 from io import BytesIO
+import uuid
 
-API_KEY = "ここにAPIキー"
-API_SECRET = "ここにAPIシークレット"
-MERCHANT_ID = "ここに加盟店ID"
+from .models import PaymentHistory, Plan
+
+API_KEY = "a_rMJ9p06Bc1_5dse"
+API_SECRET = "8F1h8BAk2eiIj5DqQ5oVbiqOQywYCHu74+Nd1dLPQfo="
+MERCHANT_ID = "469444504158363648"
 
 client = paypayopa.Client(auth=(API_KEY, API_SECRET), production_mode=False)
 client.set_assume_merchant(MERCHANT_ID)
 
+
 def index(request):
-    return render(request,"checkout.html")
+    return render(request, "checkout.html")
+
 
 @csrf_exempt
 def redirect_to_paypay(request):
+    # ↓本来は引数から受け取る
+    uid = "hf398h9gh93rgh9h" 
+    plan = "UT"
+    # ↑
+    merchantPaymentId = str(uuid.uuid4().hex)
+    PaymentHistory.objects.create(uid=uid,plan=Plan.objects.get(plan_name=plan),merchantPaymentId=merchantPaymentId)
     req = {
-        "merchantPaymentId": "mercdddhant_payment_idd_dddds_d",
+        "merchantPaymentId": merchantPaymentId,
         "codeType": "ORDER_QR",
         "redirectUrl": "http://localhost:4989/success",
-        "redirectType":"WEB_LINK",
-        "orderDescription":"Example - Mune Cake shop",
+        "redirectType": "WEB_LINK",
+        "orderDescription": "Example - Mune Cake shop",
         "orderItems": [{
             "name": "Moon cake",
             "category": "pasteries",
@@ -48,14 +59,15 @@ def redirect_to_paypay(request):
     print(paypay_url)
     return redirect(paypay_url)
 
+
 @csrf_exempt
 def generate_qr_code(request):
     req = {
         "merchantPaymentId": "dfasfasfdasdfadfasdfdasdfdddfas",
         "codeType": "ORDER_QR",
         "redirectUrl": "http://foobar.com",
-        "redirectType":"WEB_LINK",
-        "orderDescription":"Example - Mune Cake shop",
+        "redirectType": "WEB_LINK",
+        "orderDescription": "Example - Mune Cake shop",
         "orderItems": [{
             "name": "Moon cake",
             "category": "pasteries",
@@ -76,8 +88,9 @@ def generate_qr_code(request):
     buffer = BytesIO()
     img.save(buffer, format="PNG")
     qr = base64.b64encode(buffer.getvalue()).decode().replace("'", "")
-    param = { 'qr': qr}
+    param = {'qr': qr}
     return render(request, "qr.html", param)
+
 
 def success(request):
     return render(request, "success.html")
