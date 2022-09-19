0dc83967cf1b51ecac6045a769d19ddf5386243c
Hibagon1go <hibarayoichiro871@gmail.com>
2022-09-20 03:08:08 +0900

    static関連の設定修正

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from config.settings import common
+from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("blog/", include("blog.urls"))]
-+static(common.STATIC_URL, document_root=common.STATICFILES_DIRS)
+urlpatterns = [path("admin/", admin.site.urls), path("blog/", include("blog.urls"))] + static(
+    settings.STATIC_URL, document_root=settings.STATICFILES_DIRS
+)
