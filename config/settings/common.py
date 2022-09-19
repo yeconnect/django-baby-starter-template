6bbccbd1f4a92537924f84d0c5d6c6c4554a7a61
Hibagon1go <hibarayoichiro871@gmail.com>
2022-09-20 01:58:20 +0900

    start.sh修正

--- config/settings/common.py
+++ config/settings/common.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -105,3 +107,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo migration start
-python manage.py migrate
+python manage.py migrate --settings=settings.production
 echo migration end
 gunicorn config.wsgi:application --bind 0.0.0.0:8080
